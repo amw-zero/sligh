a8d18fdd47b6d50477be5764a6aa589454ec2e92
Alex Weisberger <alex.m.weisberger@gmail.com>
2021-10-30 20:25:47 -0400

    Parsing whole schema definition

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,9 @@ struct LangParser;
 #[derive(Debug)]
 enum AstNode {
     InvalidNode,
-    SchemaDef{ 
-        name: Box<AstNode>, 
-        body: Vec<AstNode>,
+    SchemaDef { name: Box<AstNode>, body: Box<AstNode> },
+    SchemaBody {
+        definitions: Vec<AstNode>,
     },
     Identifier(String),
     Type(Box<AstNode>),
@@ -50,22 +50,29 @@ fn attribute(pair: pest::iterators::Pair<Rule>,) -> AstNode {
     let mut inner = pair.into_inner();
     let name = identifier(inner.next().unwrap());
     let r#type = r#type(inner.next().unwrap());
+
     return AstNode::SchemaAttribute{ name: Box::new(name), r#type: Box::new(r#type) }
 }
 
-fn parse(pair: pest::iterators::Pair<Rule>, schemas: &mut HashMap<String, AstNode>) -> AstNode {
+fn parse(pair: pest::iterators::Pair<Rule>) -> AstNode {
     match pair.as_rule() {
         Rule::Schema => {
             let mut inner = pair.into_inner();
             let name = identifier(inner.next().unwrap());
-            let body = inner.map(|statement| parse(statement, schemas)).collect();
-            let schema = AstNode::SchemaDef { name: Box::new(name), body: body };
-            // schemas.insert(identifier_name(name), schema);
+            let body = parse(inner.next().unwrap());
+
+            let schema = AstNode::SchemaDef { name: Box::new(name), body: Box::new(body) };
             
             return schema;
         },
-        Rule::SchemaAttribute => attribute(pair.into_inner().next().unwrap()),
-        Rule::SchemaMethod => schema_method(pair.into_inner().next().unwrap()),
+        Rule::SchemaBody => {
+            let inner = pair.into_inner();
+            let definitions = inner.map(parse).collect();
+
+            return AstNode::SchemaBody { definitions: definitions };
+        },
+        Rule::SchemaAttribute => attribute(pair),
+        Rule::SchemaMethod => schema_method(pair),
         Rule::Identifier => AstNode::Identifier(pair.as_str().into()),
         _ => { 
             println!("Other");
@@ -77,12 +84,12 @@ fn parse(pair: pest::iterators::Pair<Rule>, schemas: &mut HashMap<String, AstNod
 fn main() {
     let source = std::fs::read_to_string("./src/test.lang").expect("Gotta exist");
     let result = LangParser::parse(Rule::Program, &source);
-    let mut schemas: HashMap<String, AstNode> = HashMap::new();
+//    let mut schemas: HashMap<String, AstNode> = HashMap::new();
     let mut statements: Vec<AstNode> = vec![];
     match result {
         Ok(pairs) => {
             for pair in pairs {
-                statements.push(parse(pair, &mut schemas))
+                statements.push(parse(pair))
             }
         },
         Err(e) => println!("Error {:?}", e)
