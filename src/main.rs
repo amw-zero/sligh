a7454bd4315d0cc9608723da398f4d035eef9776
Alex Weisberger <alex.m.weisberger@gmail.com>
2022-01-12 14:08:37 -0500

    start generating JS code

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use pest::{self, Parser};
 #[grammar = "grammar.pest"]
 struct LangParser;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum AstNode {
     InvalidNode,
     SchemaDef { name: Box<AstNode>, body: Box<AstNode> },
@@ -24,6 +24,57 @@ enum AstNode {
         name: Box<AstNode>,
         arguments: Vec<AstNode>,
     },
+}
+
+#[derive(Debug, Clone)]
+enum JSAstNode {
+    InvalidNode,
+    ClassDef { name: Box<JSAstNode>, body: Box<JSAstNode>},
+    ClassBody { },
+    Identifier(String)
+}
+
+fn js_gen_string(node: JSAstNode) -> String {
+    match node {
+        JSAstNode::ClassDef { name, body } => {
+            let class_name = js_iden_name(*name);
+            let class_body = js_class_body(*body);
+            format!("class {}\n  {}", class_name, class_body)
+        },
+        _ => "".to_string()
+    }
+}
+
+fn js_iden_name(node: JSAstNode) -> String {
+    match node {
+        JSAstNode::Identifier(name) => name,
+        _ => panic!("Invalid JS identifier")
+    }
+}
+
+fn js_class_body(node: JSAstNode) -> String {
+    match node {
+        JSAstNode::ClassBody{} => "".to_string(),
+        _ => panic!("Invalid JS class body")
+    }
+}
+
+fn js_translate(ast: AstNode) -> JSAstNode {
+    match ast {
+        AstNode::SchemaDef { name, body: _ } => 
+            JSAstNode::ClassDef { 
+                name: Box::new(js_translate_identifier(name)), 
+                body: Box::new(JSAstNode::ClassBody{})
+            },
+        _ => JSAstNode::InvalidNode        
+    }
+}
+
+fn js_translate_identifier(ast: Box<AstNode>) -> JSAstNode {
+    match *ast {
+        AstNode::Identifier(name) => JSAstNode::Identifier(name),
+        _ => JSAstNode::InvalidNode
+    }
 }
 
 fn identifier(pair: pest::iterators::Pair<Rule>) -> AstNode {
@@ -91,14 +142,20 @@ fn main() {
     let result = LangParser::parse(Rule::Program, &source);
 //    let mut schemas: HashMap<String, AstNode> = HashMap::new();
     let mut statements: Vec<AstNode> = vec![];
+    let mut js: Vec<JSAstNode> = vec![];
+    let mut js_code: Vec<String> = vec![];
     match result {
         Ok(pairs) => {
             for pair in pairs {
-                statements.push(parse(pair))
+                let parsed = parse(pair);
+                statements.push(parsed.clone());
+                let js_ast = js_translate(parsed);
+                js.push(js_ast.clone());
+                js_code.push(js_gen_string(js_ast));
             }
         },
         Err(e) => println!("Error {:?}", e)
     }
 
-    println!("{:?}", statements)
+    println!("{}", js_code[0]);
 }
