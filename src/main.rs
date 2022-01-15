e2a7ec21265ed1f6d6cca63a753c6da5d70820ed
Alex Weisberger <alex@relaypayments.com>
2022-01-14 19:24:59 -0500

    Properly parse and codegen call expressions

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,11 @@ enum AstNode {
         name: Box<AstNode>,
         arguments: Vec<AstNode>,
     },
+    CallExpr {
+        receiver: Box<AstNode>,
+        call_name: Box<AstNode>,
+        args: Vec<AstNode>,
+    },
     Expr(String),
 }
 
@@ -39,6 +44,7 @@ enum JSAstNode {
     ClassBody { definitions: Vec<JSAstNode>},
     ClassProperty { identifier: Box<JSAstNode> },
     ClassMethod { name: Box<JSAstNode>, args: Vec<JSAstNode>, body: Box<JSAstNode>},
+    CallExpr { receiver: Box<JSAstNode>, call_name: Box<JSAstNode>, args: Vec<JSAstNode> },
     Expr(String),
     Identifier(String)
 }
@@ -49,6 +55,16 @@ fn js_gen_string(node: JSAstNode) -> String {
             let class_name = js_gen_iden_name(*name);
             let class_body = js_gen_class_body(*body);
             format!("class {} {{\n  {}\n}}", class_name, class_body)
+        },
+        JSAstNode::CallExpr { receiver, call_name, args } => {
+            let receiver_name = js_gen_iden_name(*receiver);
+            let method_call = js_gen_iden_name(*call_name);
+            let mut arg_names: Vec<String> = vec![];
+            for arg in args {
+                arg_names.push(js_gen_iden_name(arg));
+            }
+            let comma_separated_args = arg_names.join(", ");
+            format!("{}.{}({})", receiver_name, method_call, comma_separated_args)
         },
         JSAstNode::Expr(e) => e,
         _ => "".to_string()
@@ -125,7 +141,6 @@ fn js_translate_class_body(ast: AstNode) -> JSAstNode {
             let mut js_definitions: Vec<JSAstNode> = vec![];
             for def in definitions {
                 js_definitions.push(js_translate(def));
-//                definitions.map(js_translate) 
             }
             JSAstNode::ClassBody { 
                 definitions: js_definitions
@@ -137,7 +152,17 @@ fn js_translate_class_body(ast: AstNode) -> JSAstNode {
 
 fn js_translate_expr(expr: AstNode) -> JSAstNode {
     match expr {
-        AstNode::Expr(e) => JSAstNode::Expr(e),
+        AstNode::CallExpr { receiver, call_name, args } => {
+            let mut js_args: Vec<JSAstNode> = vec![];
+            for arg in args {
+                js_args.push(js_translate_identifier(arg));
+            }
+            JSAstNode::CallExpr {
+                receiver: Box::new(js_translate_identifier(*receiver)),
+                call_name: Box::new(js_translate_identifier(*call_name)),
+                args: js_args,
+            }
+        },
         _ => JSAstNode::InvalidNode
     }
 }
@@ -147,6 +172,81 @@ fn js_translate_identifier(ast: AstNode) -> JSAstNode {
         AstNode::Identifier(name) => JSAstNode::Identifier(name),
         _ => JSAstNode::InvalidNode
     }
+}
+
+// Infrastructure expansion
+
+fn js_find_class_methods(node: JSAstNode) -> Vec<JSAstNode> {
+    let mut class_methods: Vec<JSAstNode> = vec![];
+    match node {
+        JSAstNode::ClassDef { body, .. } => match *body {
+            JSAstNode::ClassBody { definitions } => {
+                for def in definitions {
+                    match def {
+                        JSAstNode::ClassMethod {..}=> class_methods.push(def),
+                        _ => ()
+                    }
+                }
+            },
+            _ => ()
+        },
+        _ => ()
+    };
+
+    class_methods
+}
+
+fn js_class_method_name_expanded(method_name: JSAstNode) -> JSAstNode {
+    match method_name {
+        JSAstNode::Identifier(n) => JSAstNode::Identifier(format!("{}Client", n)),
+        _ => JSAstNode::InvalidNode
+    }
+}
+
+// These aren't real expression yet - just simple strings
+fn js_body_expand(body: JSAstNode) -> JSAstNode {
+    match body {
+        // query mapping of function name to expanded function (i.e push to fetch),
+        // and replace body with it
+        JSAstNode::Expr(e) => JSAstNode::InvalidNode, // symbol_table[e]
+        _ => JSAstNode::InvalidNode
+    }
+}
+
+fn js_expand_client(class_method: JSAstNode) -> JSAstNode {
+    match class_method {
+        JSAstNode::ClassMethod { name, args, body } => {
+            let expanded_name = Box::new(js_class_method_name_expanded(*name));
+            let expanded_body = Box::new(js_body_expand(*body.clone()));
+            JSAstNode::ClassMethod { name: expanded_name, args: args, body: body }
+        },
+        _ => JSAstNode::InvalidNode
+    }
+}
+
+// might want to write quoted JS macros here:
+// consider doing macros in MyLang, i.e. write infra in 
+// MyLang first before translating to target lang ?. Every 
+// func call / symbol in MyLang would have to be translated
+// by the backend. I.e. client.request() maps to fetch in JS.
+fn js_infra_expand(node: JSAstNode) -> JSAstNode {
+    let class_methods = js_find_class_methods(node);
+    let expanded_class_methods: Vec<JSAstNode> = vec![];
+    for cm in &class_methods {
+
+    }
+
+    let client = JSAstNode::ClassDef { 
+        name: Box::new(JSAstNode::Identifier("Client".to_string())),
+        body: Box::new(JSAstNode::ClassBody{
+            definitions: class_methods
+        }) 
+    };
+
+    let server =
+        "class Server { \n".to_string();
+
+    client
 }
 
 // Parser
@@ -193,23 +293,28 @@ fn parse(pair: pest::iterators::Pair<Rule>) -> AstNode {
 
             let schema = AstNode::SchemaDef { name: Box::new(name), body: Box::new(body) };
             
-            return schema;
+            schema
         },
         Rule::SchemaBody => {
             let inner = pair.into_inner();
             let definitions = inner.map(parse).collect();
 
-            return AstNode::SchemaBody { definitions: definitions };
+            AstNode::SchemaBody { definitions: definitions }
         },
         Rule::SchemaAttribute => attribute(pair),
         Rule::SchemaMethod => schema_method(pair),
         Rule::Identifier => AstNode::Identifier(pair.as_str().into()),
-        Rule::MethodArgs => {
-            return AstNode::MethodArgs { name: Box::new(parse(pair.into_inner().next().unwrap())), arguments: vec![] }
+        Rule::MethodDefArgs => {
+            AstNode::MethodArgs { name: Box::new(parse(pair.into_inner().next().unwrap())), arguments: vec![] }
         },
         Rule::MethodBody => parse(pair.into_inner().next().unwrap()),
         Rule::Expr => {
-            return AstNode::Expr(pair.as_str().into())
+            let mut expr = pair.into_inner();
+            AstNode::CallExpr {
+                receiver: Box::new(AstNode::Identifier(expr.next().unwrap().as_str().into())),
+                call_name:  Box::new(AstNode::Identifier(expr.next().unwrap().as_str().into())),
+                args: expr.map(|e| AstNode::Identifier(e.as_str().into())).collect(),
+            }
         },
         _ => { 
             println!("Other");
@@ -223,23 +328,36 @@ fn main() {
     let result = LangParser::parse(Rule::Program, &source);
 //    let mut schemas: HashMap<String, AstNode> = HashMap::new();
     let mut statements: Vec<AstNode> = vec![];
-    let mut js: Vec<JSAstNode> = vec![];
     let mut js_code: Vec<String> = vec![];
+    let mut js_infra_expanded: Vec<JSAstNode> = vec![];
+    let mut js_infra_code: Vec<String> = vec![];
+    let mut js_asts: Vec<JSAstNode> = vec![];
+
     match result {
         Ok(pairs) => {
             for pair in pairs {
                 let parsed = parse(pair);
+
                 statements.push(parsed.clone());
-                let js_ast = js_translate(parsed);
-                js.push(js_ast.clone());
-                js_code.push(js_gen_string(js_ast));
+                let js_ast = js_translate(parsed.clone());
+                js_asts.push(js_ast.clone());
+                js_code.push(js_gen_string(js_ast.clone()));
+                let infra_expanded = js_infra_expand(js_ast);
+                js_infra_expanded.push(infra_expanded.clone());
+                js_infra_code.push(js_gen_string(infra_expanded));
             }
         },
         Err(e) => println!("Error {:?}", e)
     }
 
-    println!("{:?}", statements);
+    for c in js_asts {
+        println!("JS: {:?}", c);
+        println!("Class methods: {:?}", js_find_class_methods(c));
+    }
     for c in js_code {
         println!("{}", c);
+    }
+    for ex in js_infra_code {
+        println!("{}", ex)
     }
 }
