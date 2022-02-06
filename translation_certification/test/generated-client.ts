0cd85ec0a8675944bb83adf09176a607ab434c40
Alex Weisberger <alex@relaypayments.com>
2022-02-06 10:14:13 -0500

    Working deterministic test with SQL data setup

--- translation_certification/test/generated-client.ts
+++ translation_certification/test/generated-client.ts
@@ -7,28 +7,27 @@ import { RecurringTransaction } from "./generated-model";
   
   recurring_transactions: RecurringTransaction[] = [];
   
-  async create_recurring_transactionClient(rt: RecurringTransaction) {
-//    console.log("Ho?")
-     /*let resp = await*/ fetch("http://localhost:3000/recurring_transactions", { method: "POST", body: JSON.stringify(rt), headers: { "Content-Type": "application/json" } });
-//     console.log({resp})
-//    let json = await resp.json();
-//    let respTrans = JSON.parse(json);
-      this.recurring_transactions.push(rt);
-   }
-  
-  delete_recurring_transactionClient(rt: RecurringTransaction) {
-    fetch("http://localhost:3000/recurring_transactions", { method: "DELETE", body: JSON.stringify(rt), headers: { "Content-Type": "application/json" } });
-  this.recurring_transactions = this.recurring_transactions.filter(rtt => rtt.name === rt.name);
-   }
-  
-  update_recurring_transactionClient(rt: RecurringTransaction) {
-    fetch("http://localhost:3000/recurring_transactions", { method: "PUT", body: JSON.stringify(rt), headers: { "Content-Type": "application/json" } });
+  async create_recurring_transaction(rt: RecurringTransaction) {
+    let resp = await fetch("http://localhost:3000/recurring_transactions", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(rt) });
+    console.log({createResponse: resp});
   this.recurring_transactions.push(rt);
    }
   
-  view_recurring_transactions() {
-    this.recurring_transactions }
+  async delete_recurring_transaction(rt: RecurringTransaction) {
+    fetch("http://localhost:3000/recurring_transactions/" + rt.id, { method: "DELETE", headers: { "Content-Type": "application/json" } });
+  this.recurring_transactions = this.recurring_transactions.filter((data) => {
+   return data.id !== rt.id });
+   }
   
+  async update_recurring_transaction(rt: RecurringTransaction) {
+    ;
+   }
   
-  }
+  async view_recurring_transactions() {
+    console.log("View recurring transactions");
+    let data = await fetch("http://localhost:3000/recurring_transactions", { method: "GET", headers: { "Content-Type": "application/json" } });
+    let json = await data.json();
+  this.recurring_transactions = json;
+   }
+}
 
--- translation_certification/test/translation.test.ts
+++ translation_certification/test/translation.test.ts
@@ -3,15 +3,26 @@ import 'mocha';
 import { Budget as Fullstack,  } from "./generated-client";
 import { Budget as Model, RecurringTransaction } from "./generated-model"
 import fc from 'fast-check';
+import { Database } from "sqlite3";
+import { omit } from "lodash";
+
+function withoutId(rt: RecurringTransaction): Omit<RecurringTransaction, "id"> {
+  return omit(rt, "id");
+}
 
 class CreateCommand implements fc.AsyncCommand<Model, Fullstack> {
   constructor(readonly value: RecurringTransaction) {}
   check = (m: Readonly<Model>) => true;
   async run(m: Model, r: Fullstack): Promise<void> {
-    await r.create_recurring_transactionClient(this.value);
+    await r.create_recurring_transaction(this.value);
+    await r.view_recurring_transactions();
     m.create_recurring_transaction(this.value);
 
-    expect(r.recurring_transactions).to.deep.eq(m.recurring_transactions);
+    console.log("Fullstack:")
+    console.log(r.recurring_transactions);
+    console.log("Model:")
+    console.log(m.recurring_transactions);
+    expect(r.recurring_transactions.map(withoutId)).to.deep.eq(m.recurring_transactions.map(withoutId));
   }
   toString = () => `createRecurringTransaction(${this.value})`;
 }
@@ -21,8 +32,10 @@ class DeleteCommand implements fc.AsyncCommand<Model, Fullstack> {
     return true;
   }
   async run(m: Model, r: Fullstack): Promise<void> {
-    r.delete_recurring_transactionClient(this.value)
+    await r.delete_recurring_transaction(this.value)
+    await r.view_recurring_transactions();
     m.delete_recurring_transaction(this.value);
+
 
     expect(r.recurring_transactions).to.deep.eq(m.recurring_transactions);
   }
@@ -39,19 +52,35 @@ describe('FullstackBudget', function() {
 
         return new CreateCommand(rt);
       }),
-      fc.record({ name: fc.string(), amount: fc.float()}).map(({ name, amount }) => {
-        let rt = new RecurringTransaction();
-        rt.name = name;
-        rt.amount = amount;
+      // fc.record({ name: fc.string(), amount: fc.float()}).map(({ name, amount }) => {
+      //   let rt = new RecurringTransaction();
+      //   rt.name = name;
+      //   rt.amount = amount;
 
-        return new DeleteCommand(rt);
-      }),
+      //   return new DeleteCommand(rt);
+      // }),
     ];
+
+    const db = new Database('../web_server/test.db');
 
     await fc.assert(
       fc.asyncProperty(fc.commands(cmds, { maxCommands: 20 }), cmds => {
+        console.log("\n\n==== Property iteration ====");
         const s = () => ({ model: new Model(), real: new Fullstack(() => {}) });
-        return fc.asyncModelRun(s, cmds);
+        let setupPromise = new Promise<boolean | void>((resolve, reject) => {
+          db.run("DELETE FROM recurring_transactions", (result, err) => {
+            console.log("Data setup")
+            if (err) {
+              console.log("Data error");
+              reject(err);
+            } else {
+              console.log("Running model");
+              resolve(fc.asyncModelRun(s, cmds));
+            }
+          });
+        });
+        
+        return setupPromise;
       }),
       { numRuns: 25 },
     );
