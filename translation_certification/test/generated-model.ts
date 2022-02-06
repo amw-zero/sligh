80fb0ba9ed00577246c5c3c332bd001b993df755
Alex Weisberger <alex.m.weisberger@gmail.com>
2022-02-06 10:14:13 -0500

    Working deterministic test with SQL data setup

--- translation_certification/test/generated-model.ts
+++ translation_certification/test/generated-model.ts
@@ -1,22 +1,15 @@
 export class RecurringTransaction {
     amount: number = 0;
   name: string = "";
-
+  id: number = 0;
   }
-  export class Budget {
+
+export  class Budget {
     recurring_transactions: RecurringTransaction[] = [];
   create_recurring_transaction(rt: RecurringTransaction) {
     this.recurring_transactions.push(rt) }
-
+  
   delete_recurring_transaction(rt: RecurringTransaction) {
-    this.recurring_transactions = this.recurring_transactions.filter(rtt => rt.name === rtt.name)
-    }
-
-//   update_recurring_transaction(rt: RecurringTransaction) {
-//     this.recurring_transactions.update!(rt) }
-
-  view_recurring_transactions() {
-    this.recurring_transactions }
-
-
+    this.recurring_transactions = this.recurring_transactions.filter((data) => {
+    return data.id !== rt.id }) }
   }
--- web_server/define-endpoints.js
+++ web_server/define-endpoints.js
@@ -4,6 +4,10 @@ function defineEndpoints(app, db) {
      db.run("INSERT INTO recurring_transactions (amount, name) VALUES (?, ?)", [data.amount, data.name]);
      res.send({  });
     });
+
+    app.get("/recurring_transactions", (req, res) => {
+      db.all("SELECT * FROM recurring_transactions", (_, rows) => {
+      res.send(rows) }) });
 }
   
 module.exports = defineEndpoints;
