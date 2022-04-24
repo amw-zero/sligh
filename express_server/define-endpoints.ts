601f706f83230223157cac425d64b7c972e370e8
Alex Weisberger <alex@relaypayments.com>
2022-04-23 22:34:17 -0400

    Commit recent server compilation

--- express_server/define-endpoints.ts
+++ express_server/define-endpoints.ts
@@ -4,15 +4,28 @@ interface CreateRecurringTransaction { amount: number;name: string }
 
 interface ScheduledTransaction { name: string }
 
-interface Filter { ids: number[] }
+interface ScenarioRecurringTransaction { id: number;amount: number;name: string }
+
+interface Scenario { id: number;name: string;scenarioRecurringTransactions: ScenarioRecurringTransaction[] }
+
+interface CreateScenarioRequest { name: string }
+
+interface CreateScenarioRecurringTransaction { amount: number;name: string }
+
+interface CreateScenario { name: string;scenarioRecurringTransactions: CreateScenarioRecurringTransaction[] }
 
 function expand(rt: RecurringTransaction) {
   return { name: rt.name };
 
 }
 
+function toScenarioTransaction(recurringTransaction: RecurringTransaction) {
+  return { amount: recurringTransaction.amount, name: recurringTransaction.name };
+
+}
+
 export function defineEndpoints(app, db) {
-  app.post("/recurring_transactions", (req, res) => {
+  app.post("/recurringTransactions", (req, res) => {
  let data = req.body;
 db.serialize(() => {
  db.run("INSERT INTO recurring_transactions (amount, name) VALUES (?, ?)", [data.amount, data.name]);
@@ -20,20 +33,28 @@ db.get("SELECT last_insert_rowid()", (err, row) => {
  res.send({ amount: data.amount, name: data.name, id: row["last_insert_rowid()"] }) });
  });
  });
-app.delete("/recurring_transactions/:id", (req, res) => {
+app.delete("/recurringTransactions/:id", (req, res) => {
  db.run("DELETE FROM recurring_transactions WHERE id = ?", [req.params.id]);
 res.send({  });
  });
-app.put("/recurring_transactions/:id", (req, res) => {
+app.put("/recurringTransactions/:id", (req, res) => {
  res.send({  }) });
-app.get("/recurring_transactions", (req, res) => {
+app.get("/recurringTransactions", (req, res) => {
  db.all("SELECT * FROM recurring_transactions", (_, rows) => {
  res.send(rows);
  }) });
-app.get("/scheduled_transactions", (req, res) => {
+app.get("/scheduledTransactions", (req, res) => {
  db.all("SELECT * FROM recurring_transactions", (_, rts) => {
- let scheduled_transactions = rts.map(expand);
-res.send(scheduled_transactions);
+ let scheduledTransactions = rts.map(expand);
+res.send(scheduledTransactions);
  }) });
+app.post("/scenarios", (req, res) => {
+ let data = req.body;
+db.serialize(() => {
+ db.run("INSERT INTO scenarios (name) VALUES (?)", [data.name]);
+db.get("SELECT last_insert_rowid()", (err, row) => {
+ res.send({ name: data.name, scenarioRecurringTransactions: [], id: row["last_insert_rowid()"] }) });
+ });
+ });
 
 }
--- express_server/dist/define-endpoints.js
+++ express_server/dist/define-endpoints.js
@@ -4,8 +4,11 @@ exports.defineEndpoints = void 0;
 function expand(rt) {
     return { name: rt.name };
 }
+function toScenarioTransaction(recurringTransaction) {
+    return { amount: recurringTransaction.amount, name: recurringTransaction.name };
+}
 function defineEndpoints(app, db) {
-    app.post("/recurring_transactions", (req, res) => {
+    app.post("/recurringTransactions", (req, res) => {
         let data = req.body;
         db.serialize(() => {
             db.run("INSERT INTO recurring_transactions (amount, name) VALUES (?, ?)", [data.amount, data.name]);
@@ -14,22 +17,31 @@ function defineEndpoints(app, db) {
             });
         });
     });
-    app.delete("/recurring_transactions/:id", (req, res) => {
+    app.delete("/recurringTransactions/:id", (req, res) => {
         db.run("DELETE FROM recurring_transactions WHERE id = ?", [req.params.id]);
         res.send({});
     });
-    app.put("/recurring_transactions/:id", (req, res) => {
+    app.put("/recurringTransactions/:id", (req, res) => {
         res.send({});
     });
-    app.get("/recurring_transactions", (req, res) => {
+    app.get("/recurringTransactions", (req, res) => {
         db.all("SELECT * FROM recurring_transactions", (_, rows) => {
             res.send(rows);
         });
     });
-    app.get("/scheduled_transactions", (req, res) => {
+    app.get("/scheduledTransactions", (req, res) => {
         db.all("SELECT * FROM recurring_transactions", (_, rts) => {
-            let scheduled_transactions = rts.map(expand);
-            res.send(scheduled_transactions);
+            let scheduledTransactions = rts.map(expand);
+            res.send(scheduledTransactions);
+        });
+    });
+    app.post("/scenarios", (req, res) => {
+        let data = req.body;
+        db.serialize(() => {
+            db.run("INSERT INTO scenarios (name, scenarioRecurringTransactions) VALUES (?, ?)", [data.name, data.scenarioRecurringTransactions]);
+            db.get("SELECT last_insert_rowid()", (err, row) => {
+                res.send({ name: data.name, scenarioRecurringTransactions: data.scenarioRecurringTransactions, id: row["last_insert_rowid()"] });
+            });
         });
     });
 }
