cbe5864e3bc39d512a197f0ff573e62848f7ab88
Alex Weisberger <alex@relaypayments.com>
2022-09-30 21:53:10 -0400

    Handle nested TS blocks

--- lib/core.ml
+++ lib/core.ml
@@ -16,24 +16,10 @@ let rec tsexpr_of_expr e = match e with
   | Let(v, b) -> TSLet(v, tsexpr_of_expr b)
   | Iden (s) -> TSIden(s)
   | Num(n) -> TSNum(n)
-  (* Don't know what to do with nested TS blocks, and boolexp isn't even real *)
+  | TS(t) -> t
   | _ -> TSNum(-1)
 
 type decl = Idk
 
 type syntax = 
   Expr of expr | Decl of decl
-
-(* 
-
-let prog = typescript:
-  let x = 5;
-  let y = 17;
-end
-
-let x = 5
-typescript:
-  let tx = `x`;
-end
-
-*)
