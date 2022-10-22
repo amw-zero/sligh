8512e3039810421bc0ff01ad7d5ded1cc97af796
Alex Weisberger <alex@relaypayments.com>
2022-10-22 00:17:12 -0400

    Progress on the proof of concept state lens optimization

--- State.thy
+++ State.thy
@@ -118,6 +118,8 @@ record ('s, 'e) dt =
 definition exec_dt :: "('s, 'e) dt \<Rightarrow> 'e list \<Rightarrow> 's" where
 "exec_dt dt i = foldl (\<lambda>s e. (step dt) e s) (state dt) i"
 
+definition "fw_sim I M = (\<forall>s s' es. exec_dt I es s = s'\<longrightarrow> exec_dt M es s = s')" 
+
 definition "point_step e p = (case e of
   TranslateX x \<Rightarrow> p\<lparr> X := (X p) + x \<rparr> 
 | TranslateY y \<Rightarrow> p\<lparr> Y := (Y p) + y \<rparr>)"
@@ -154,5 +156,139 @@ next
     by (auto simp: algebra_simps)
 qed
 
+datatype View =
+  TranslateXView int
+  | TranslateYView int
+
+record ('s, 'e, 'v) datatype_lens =
+  lens_state :: 's
+  lens_step :: "('e \<Rightarrow> ('s, 'v) lens) \<Rightarrow> 'e \<Rightarrow> 's \<Rightarrow> 's"
+
+type_synonym ('e, 's) dt_step = "'e \<Rightarrow> 's \<Rightarrow> 's"
+
+type_synonym ('e, 's, 'v) lens_mapping = "'e \<Rightarrow> ('s, 'v) lens"
+type_synonym ('e, 'v) view_func_mapping = "'e \<Rightarrow> ('v \<Rightarrow> 'v)"
+
+type_synonym ('e, 's, 'v) dt_step_transformer = "'e \<Rightarrow> ('e, 's, 'v) lens_mapping \<Rightarrow> ('e, 's) dt_step"
+
+
+definition "x_view p = TranslateXView (X p)"
+text "Had bug here where the y_view impl returned TranslateXView instead"
+definition "y_view p = TranslateXView (Y p)"
+
+definition "lens_put v p = (case v of
+  TranslateXView x \<Rightarrow> p\<lparr> X := x \<rparr>
+  | TranslateYView y \<Rightarrow> p\<lparr> Y := y \<rparr>)"
+
+definition map_lens :: "point_action \<Rightarrow> (Point, View) lens" where
+"map_lens e = (case e of
+  TranslateX x \<Rightarrow> \<lparr> Get=x_view, Put=lens_put \<rparr>
+  | TranslateY y \<Rightarrow> \<lparr>Get=y_view, Put=lens_put\<rparr>)"
+
+definition "translate_view i = (\<lambda>v. (case v of
+  TranslateXView x \<Rightarrow> TranslateXView (translateXInt i x)
+  | TranslateYView y \<Rightarrow> TranslateYView (translateYInt i y)))"
+
+definition "map_view_fun e = (case e of
+  TranslateX x \<Rightarrow> translate_view x
+  | TranslateY y \<Rightarrow> translate_view y)"
+
+text "This is a generic combinator from lens and view func mappings to a step function"
+
+definition lens_step :: "('e, 's, 'v) lens_mapping \<Rightarrow> ('e, 'v) view_func_mapping \<Rightarrow> ('e, 's) dt_step"  where
+"lens_step lm vfn = (\<lambda>e s.(
+  let lns = lm e in
+  let v = (Get lns) s in
+  let view_op = vfn e in
+  let res = view_op v in
+  
+  (Put lns) res s
+))"
+
+definition optimized_step :: "point_action \<Rightarrow> Point \<Rightarrow> Point" where
+"optimized_step = lens_step    map_lens    map_view_fun"
+  
+definition "optimized_dt = \<lparr> state = initPoint, step = optimized_step \<rparr>"
+
+value "(step optimized_dt) (TranslateX (-1)) (state optimized_dt)"
+
+value "exec_dt optimized_dt [TranslateX (-1), TranslateY 0]"
+
+theorem "exec_dt point_dt2 es = exec_dt optimized_dt es"
+  sorry
+
+section "Substate Mapping"
+
+text "A sub data type consists of a lens on the state and
+      a function that modifies the view that's projected from the 
+      lens. This view is meant to have one variant per event in 
+      the original data type, where each variant represents the 
+      subset of the state that that event needs to carry out its
+      operation."
+      
+record ('s, 'v) sub_dt =
+  sdlens :: "('s, 'v) lens"
+  sdview_func :: "'v \<Rightarrow> 'v"
+
+text "The substate_mapping generates a sub data type given an event"
+
+type_synonym ('e, 's, 'v) substate_mapping = "'e \<Rightarrow> ('s, 'v) sub_dt"
+
+definition substate_step :: "('e, 's, 'v) substate_mapping \<Rightarrow> ('e, 's) dt_step"  where
+"substate_step ssmap = (\<lambda>e s.(
+  let sub_dt = ssmap e in
+  let lns = (sdlens sub_dt) in
+  let vfn = (sdview_func sub_dt) in
+  let v = (Get lns) s in
+  let res = vfn v in
+  
+  (Put lns) res s
+))"
+
+text "Show a sufficient condition / proof obligation for proving that an optimized datatype
+     will always have the same execution as an unoptimized version. Show the left hand side of
+     the implication, and you know the dts are equivalent.
+    
+    Has to be some kind of relation between the lens and view_func.
+
+    Fundmentally looking for something that gets rid of the 's - the assumption is that states of
+    type 's are always large, even in the model.
+
+    As long as the lens is _well_behaved_, then an optimized datatype derived using the substate_step
+    combinator has _equal_ execution.
+  
+    The value proposition of this method is that writing a well behaved lens is relatively easy,
+    and once you have that you can test drastically reduced subsets of the state space and get the
+    same simulation and refinement guarantees since the lens-optimized data type has the exact same
+    execution as the original."
+
+definition "put_get lns v s = (
+  let get = Get lns in
+  let put = Put lns in
+  get (put v s) = v
+)"
+
+definition "get_put lns  s = (
+  let get = Get lns in
+  let put = Put lns in
+  put (get s) s = s 
+)"
+
+definition "well_behaved lns v s = (put_get lns v s \<and> get_put lns s)"
+
+theorem "\<lbrakk> 
+  well_behaved lns v s; 
+  substate_mapping = (\<lambda>e. \<lparr>sdlens=lns, sdview_func=f \<rparr>);
+  optimized = \<lparr> state=s, step=substate_step substate_mapping\<rparr> 
+\<rbrakk> \<Longrightarrow> exec_dt dt1 es = exec_dt optimized es"
+  unfolding well_behaved_def get_put_def put_get_def substate_step_def exec_dt_def
+proof(induction es arbitrary: f s v lns)
+  case Nil
+  then show ?case unfolding Let_def apply auto sledgehammer sorry
+  
+next
+  case (Cons a es)
+  then show ?case sorry
+qed
 
 end
