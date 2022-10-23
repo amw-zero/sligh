f9e3690727349a4cdb361eda6e1c09f7350fb05b
Alex Weisberger <alex@relaypayments.com>
2022-10-23 18:21:15 -0400

    Remove example programs.

--- bin/main.ml
+++ bin/main.ml
@@ -38,98 +38,12 @@ end
 
 |} *)
 
-(* let eval = {|
-// Model
-entity Todo:
-  name: String
-end
-
-domain Test:
-  todos: Todo[]
-
-  def addTodo(t: Todo):
-    todos.create!(t)
-  end
-end
-
-// Effect definition - how is an effect executed
-// across implementation components?
-effect create!<'a>(state: SystemState<'a>, val: 'a):
-  model: 
-    state.push(val)
-  end
-
-  // Client code generated from 
-  client:
-    typescript:
-      let resp = fetch({{ state.name }}, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(val),
-      });
-      let data = await resp.json();
-
-      client.todos.push(data);
-    end
-  end
-
-  server:
-    // Generate endpoint code
-  end
-end
-
-// Helpers to 
-def toTsTypedIden(var: Variable)
-  typescript:
-    {{ var.name }}: {{ var.type }}
-  end
-end
-
-def toTsAction(act: Action)
-  typescript:
-    async {{ act.name }}({{ act.args.map(toTsTypedIden) }}) {
-      {{ act.body }}
-    }
-  end
-end
-
-// Generates a client.ts file
-process client:
-  typescript:
-    class Client {
-      {{ Model.variables.map(toTsTypedIden) }}
-
-      {{ Model.actions.map(toTsAction) }}
-    }
-  end
-end
-
-// Generates a server.ts file
-process server:
-  // generate web server bootstrap
-end
-|} *)
-
-(* 
-env = key -> value
-Model = {
-  Other: {
-    val: Int
-  },
-  Todo: {
-    other: Other
-  }
-}
-*)
-
 let eval = {|
 entity Todo:
   note: String
 end
 
-entity SomethingElse:
-  property: Int
-end
+
 
 def toName(a: Schema):
   a.name
