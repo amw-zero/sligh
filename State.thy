87d44cfdfe602e5ddf4b826ee03cd9b0d16181f0
Alex Weisberger <alex@relaypayments.com>
2022-10-31 15:19:30 -0400

    More thinking about the correctness statement

--- State.thy
+++ State.thy
@@ -34,8 +34,6 @@ record Point =
   X :: int
   Y :: int
 
-print_theorems
-
 definition "translateX x p = p\<lparr> X := (X p) + x \<rparr>"
 definition "translateY y p = p\<lparr> Y := (Y p) + y \<rparr>" 
 definition "initPoint = \<lparr> X=0, Y=0 \<rparr>"
@@ -91,8 +89,6 @@ record ('s, 'e) dt =
 
 definition exec_dt :: "('s, 'e) dt \<Rightarrow> 'e list \<Rightarrow> 's" where
 "exec_dt dt i = foldl (\<lambda>s e. (step dt) e s) (state dt) i"
-
-definition "fw_sim I M = (\<forall>s s' e. (step I) e s = s'\<longrightarrow> (step M) e s = s')" 
 
 definition "point_step e p = (case e of
   TranslateX x \<Rightarrow> p\<lparr> X := (X p) + x \<rparr> 
@@ -174,8 +170,8 @@ text "The substate_mapping generates a sub data type given an event"
 
 type_synonym ('e, 's, 'v) substate_mapping = "'e \<Rightarrow> ('s, 'v) sub_dt"
 
-definition substate_mapping_to_step :: "('e, 's, 'v) substate_mapping \<Rightarrow> ('e, 's) dt_step"  where
-"substate_mapping_to_step ssmap = (\<lambda>e s.(
+definition compose_substates :: "('e, 's, 'v) substate_mapping \<Rightarrow> ('e, 's) dt_step"  where
+"compose_substates ssmap = (\<lambda>e s.(
   let sub_dt = ssmap e in
   let lns = (sdlens sub_dt) in
   let vfn = (sdview_func sub_dt) in
@@ -218,16 +214,29 @@ text "If we have the view-level functions which instead of mapping a global stat
       lens between global state and view given an event, the view state function, and the global state
       dt and optmized dts can be derived from that."
 
+definition test :: "int \<Rightarrow> point_action" where "test =  TranslateX"
+
+definition "take_step e dt = (step dt) e (state dt)"
 
 theorem "\<lbrakk>
-  substate_mapping = (\<lambda>e. \<lparr>sdlens=lns, sdview_func=vfn \<rparr>);
-  gs_dt = \<lparr> state = s, step=substate_mapping_to_step substate_mapping \<rparr>;
-  vs_dt = \<lparr> state=(Get lns) s, step=(\<lambda>e s. vfn s) \<rparr>
-\<rbrakk> \<Longrightarrow> (step gs_dt) e (state gs_dt) = (Put lns) ((step vs_dt) e (state vs_dt)) s"
-  unfolding substate_mapping_to_step_def
-  by simp
+  substate_mapping = (\<lambda>e. ssm);
+  vfn = sdview_func ssm;
+  lns = sdlens ssm;
+  gs_dt = \<lparr> state = s, step=compose_substates substate_mapping \<rparr>;
+  ss_dt = \<lparr> state=(Get lns) s, step=(\<lambda>e s. vfn s) \<rparr>
+\<rbrakk> \<Longrightarrow> take_step e gs_dt = (Put lns) (take_step e ss_dt) s"
+  unfolding compose_substates_def take_step_def
+  by (metis (no_types, lifting) dt.select_convs(1) dt.select_convs(2))
 
-section "Well-behavedness"
+theorem "\<lbrakk>take_step e dt1 = take_step e dt2\<rbrakk> \<Longrightarrow> (exec_dt dt1 es) = (exec_dt dt2 es)" 
+  sorry
+
+text "Correctness - dt2 refines dt1, or exec_dt dt2 \<longrightarrow> exec_dt dt1.
+      Can be shown by simulation (forward), which involves reasoning about
+      global states. Create a substate mapping such that taking a step in the
+      substate mapping implies forward simulation."
+
+section "Well-behavedness of lenses"
 
 definition "put_get lns v s = (
   let get = Get lns in
@@ -255,7 +264,7 @@ definition "point_substate_mapping e = (case e of
 theorem "well_behaved (sdlens (point_substate_mapping e)) v s"
   sorry
 
-definition "optimized_point_step = substate_step point_substate_mapping"
+definition "optimized_point_step = compose_substates point_substate_mapping"
 definition "optimized_point_dt = \<lparr> state=initPoint, step=optimized_point_step \<rparr>"
 
 theorem "exec_dt point_dt2 es = exec_dt optimized_point_dt es"
@@ -263,26 +272,52 @@ unfolding exec_dt_def and point_dt2_def and point_dt_lens2_def and point_step_de
     and point_step_lens_def translateXInt_def translateYInt_def
     and initPoint_def and xlens_def and ylens_def and updateX_def and updateY_def and getX_def
     and getY_def and Let_def and optimized_point_dt_def and optimized_point_step_def 
-    and point_substate_mapping_def and substate_step_def and lens_put_def and x_view_def and y_view_def
+    and point_substate_mapping_def and compose_substates_def and lens_put_def and x_view_def and y_view_def
     and translate_view_def
 proof(induction es)
   case Nil
-  then show ?case by simp
+  then show ?case sorry
 next
   case (Cons a es)
-  then show ?case apply auto sorry
+  then show ?case  sorry
 qed
 
-section "Solution via data refinement"
+section "Global processes exhibit  data refinement"
 
-record ('s, 'v, 'e) dt_dr =
-  init :: "'s \<Rightarrow> 'v"
-  step :: "'e \<Rightarrow> 'v \<Rightarrow> 'v"
-  fin :: "'v \<Rightarrow> 's"
+record ('ext, 's, 'e) dt_dr =
+  init :: "'ext \<Rightarrow> 's"
+  step :: "'e \<Rightarrow> 's \<Rightarrow> 's"
+  fin :: "'s \<Rightarrow> 'ext"
 
-definition "exec_dt_dr dt s es = foldl (\<lambda>state e. (step dt) e ((init dt) state)) s es"
+definition "exec_dt_dr dt s es = foldl (\<lambda>v e. (step dt) e v) ((init dt) s) es"
 
-definition "view_get s = 5" 
+definition "refines C M = (\<forall>s s' es. exec_dt_dr C s es = s' \<longrightarrow> exec_dt_dr M s es = s')"
+
+definition "action_refines vfn_c vfn_m = (\<forall>v v'. vfn_c v = v' \<longrightarrow> vfn_m v = v')"
+
+theorem "\<lbrakk>  
+  model_proc = \<lparr> init = (\<lambda>ex. s), step=compose_substates spmm, fin=(\<lambda>s. ext) \<rparr>; 
+  impl_proc = \<lparr> init = (\<lambda>ex. s), step=compose_substates spmi, fin=(\<lambda>s. ext) \<rparr>;
+  
+  (\<forall>e. 
+    let vfn_m = sdview_func (spmm e) in
+    let lns_m = sdlens (spmm e) in
+    let vfn_i = sdview_func (spmi e) in
+    let lns_i = sdlens (spmi e) in
+    action_refines vfn_i vfn_m)
+\<rbrakk> \<Longrightarrow> refines impl_proc model_proc"
+  unfolding compose_substates_def refines_def action_refines_def exec_dt_dr_def
+  sorry
+
+(*text "SR is a relation between states in I and M, i.e. SR \<subseteq> 's x 's"
+
+definition "fw_sim SR I M = 
+  (\<forall>ex s. init I ex = s \<longrightarrow>   (\<forall>s s' e. (step I) e s = s'\<longrightarrow> (step M) e s = s')" *)
+
+section "Data refinement point translation"
+
+text "No way to create a function from 's \<Rightarrow> 'v for substates because that depends on 'e"
+(*definition "point_dt_dr = \<lparr> init = \<lambda>p. ? ? \<rparr>*)
 
 (*
 
