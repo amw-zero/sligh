72ece79d7e64d5a0db14a16f89ef1e475e942169
alex.weisberger@datadoghq.com <alex.weisberger@datadoghq.com>
2023-08-21 22:47:13 -0400

    Fix some assumptions, show that they are not vacuous.
    By verifying the simulation between two equivalent action mappings.

--- LocalActions.thy
+++ LocalActions.thy
@@ -18,15 +18,18 @@ record ('n, 's) lens  =
 text "Well-behaved lenses must follow the 'lens laws' which ensure that they retrieve and update
       the same part of the source state."
 
+definition "var_names s = {(fst v) | v. v \<in> s}"
+
+definition "extract_subvars s vs = {v | v. v \<in> s \<and> (fst v) \<in> vs }"
+
+definition "restore_subvars s vs = s - {v | v. v \<in> s \<and> (fst v) \<in> var_names vs } \<union> vs"
+
 definition "local_lens l s lv \<equiv>
   ((Get l s) \<subseteq> s
-  \<and> (Put l) lv s = s - (Get l s) \<union> lv)"
+  \<and> (Put l) lv s = restore_subvars s lv)"
 
-definition "well_behaved l \<equiv> (\<forall>ls lv lvv.
-  local_lens l ls lv \<and> 
-  (Put l) ((Get l) ls) ls = ls \<and> 
-  (Get l) ((Put l) lv ls) = lv \<and> 
-  (Put l) lvv ((Put l) lv ls) = (Put l) lvv ls)"
+definition "well_behaved l \<equiv> (\<forall>ls lv.
+  local_lens l ls lv)"
 
 text "A process is a representation of a program that proceeds through a sequence of states in 
       response to actions (inputs) of type 'e, i.e. a state machine."
@@ -84,8 +87,9 @@ theorem local_sim_imp_sim:
   and "local_simulates am_i am_m e s"
   shows "simulates (compose_local_actions am_i) (compose_local_actions am_m) e s t"
   using assms
-  unfolding simulates_def local_simulates_def compose_local_actions_def well_behaved_def local_lens_def Let_def
-  by (metis sup.idem sup_bot.right_neutral)
+  unfolding simulates_def local_simulates_def compose_local_actions_def well_behaved_def
+    local_lens_def restore_subvars_def var_names_def
+  by metis
 
 type_synonym ('s) invariant_func = "'s \<Rightarrow> bool"
 
@@ -111,5 +115,124 @@ theorem local_inv_imp_inv:
   using assms
   unfolding local_invariant_def compose_local_actions_def
   by metis
+
+section "Example"
+
+definition "read s v = { snd var | var. var \<in> s \<and> fst var = v }"
+
+datatype PointEvt = TranslateX int | TranslateY int
+
+definition "initial_state = {(''x'', 0), (''y'', 0)}"
+
+(* f is an update function *)
+definition "write s n f =
+  s 
+    - {var | var. (fst var) = n \<and> var \<in> s}
+    \<union> {(n, f (snd var)) | var. (fst var) = n \<and> var \<in> s}"
+
+definition "model_action_mapping e = (case e of
+  TranslateX n \<Rightarrow> 
+    let lens = \<lparr> 
+      Get=(\<lambda>s. extract_subvars s {''x''}),
+      Put=(\<lambda>v s. restore_subvars s v) 
+    \<rparr> in  
+    let step = (\<lambda>s. write s ''x'' (\<lambda>v. v + n)) in
+
+    \<lparr> lens=lens, step=step \<rparr>
+  | TranslateY n \<Rightarrow>
+     let lens = \<lparr> 
+      Get=(\<lambda>s. extract_subvars s {''y''}),
+      Put=(\<lambda>v s. restore_subvars s v) 
+    \<rparr> in  
+    let step = (\<lambda>s. write s ''y'' (\<lambda>v. v + n)) in
+
+    \<lparr> lens=lens, step=step \<rparr>)"
+
+definition "impl_action_mapping e = (case e of
+  TranslateX n \<Rightarrow> 
+    let lens = \<lparr> 
+      Get=(\<lambda>s. extract_subvars s {''x''}),
+      Put=(\<lambda>v s. restore_subvars s v) 
+    \<rparr> in  
+    let step = (\<lambda>s. write s ''x'' (\<lambda>v. v + n)) in
+
+    \<lparr> lens=lens, step=step \<rparr>
+  | TranslateY n \<Rightarrow>
+     let lens = \<lparr> 
+      Get=(\<lambda>s. extract_subvars s {''y''}),
+      Put=(\<lambda>v s. restore_subvars s v) 
+    \<rparr> in  
+    let step = (\<lambda>s. write s ''y'' (\<lambda>v. v + n)) in
+
+    \<lparr> lens=lens, step=step \<rparr>)"
+
+lemma ex_sub: "extract_subvars s v \<subseteq> s"
+  by (simp add: extract_subvars_def)
+
+lemma get_subset_mod: "Get (lens (model_action_mapping e)) s \<subseteq> s"
+  unfolding model_action_mapping_def
+  apply (case_tac e)
+   apply(auto)
+  using extract_subvars_def apply blast+
+  done
+
+lemma put_restores_mod:
+  "Put (lens (model_action_mapping e)) lv s = restore_subvars s lv"
+  unfolding model_action_mapping_def
+  apply (case_tac e)
+   apply(auto)
+  done
+
+lemma get_subset_impl: "Get (lens (impl_action_mapping e)) s \<subseteq> s"
+  unfolding impl_action_mapping_def
+  apply (case_tac e)
+   apply(auto)
+  using extract_subvars_def apply blast+
+  done
+
+lemma put_restores_impl:
+  "Put (lens (impl_action_mapping e)) lv s = restore_subvars s lv"
+  unfolding impl_action_mapping_def
+  apply (case_tac e)
+   apply(auto)
+  done
+
+lemma 
+  shows mod_loc_lens: "local_lens (lens (model_action_mapping e)) s lv"
+  unfolding local_lens_def
+  apply(rule conjI)
+   apply(simp add: get_subset_mod)
+  apply(simp add: put_restores_mod)
+  done
+
+lemma 
+  shows impl_loc_lens: "local_lens (lens (impl_action_mapping e)) s lv"
+  unfolding local_lens_def
+  apply(rule conjI)
+   apply(simp add: get_subset_impl)
+  apply(simp add: put_restores_impl)
+  done
+
+
+lemma mod_well_b: "well_behaved (lens (model_action_mapping e))"
+  apply (unfold well_behaved_def)
+  apply (simp add: mod_loc_lens)
+  done
+
+lemma impl_well_b: "well_behaved (lens (impl_action_mapping e))"
+  apply (unfold well_behaved_def)
+  apply (simp add: impl_loc_lens)
+  done
+
+lemma loc_sim: "local_simulates impl_action_mapping model_action_mapping e s"
+  by (simp add: impl_action_mapping_def local_simulates_def model_action_mapping_def)
+
+theorem "simulates (compose_local_actions impl_action_mapping)
+  (compose_local_actions model_action_mapping) e s t"
+  apply (rule local_sim_imp_sim)
+    apply(simp add: impl_well_b)
+   apply(simp add: mod_well_b)
+  apply(simp add: loc_sim)
+  done
 
 end
